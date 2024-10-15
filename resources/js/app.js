52e2f388a521f87e961de4fce68bdc5558d1b1ca
andrearomeo04 <pisco004@gmail.com>
2024-10-15 16:16:37 +0200

    [UPD] created type rsmc

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,18 +5,3 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
-
-const buttons = document.querySelectorAll('.delete-project')
-buttons.forEach((button) => {
-    button.addEventListener('click', function(e){
-        e.preventDefault();
-        
-        const modal = document.getElementById('deleteProjectModal');
-        const bootstrap_modal = new bootstrap.Modal(modal);
-
-        bootstrap_modal.show()
-        document.querySelector('.confirm-delete').addEventListener('click', function() {
-            button.parentElement.submit()
-        })
-    })
-})
