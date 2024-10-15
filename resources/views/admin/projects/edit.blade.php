ce96387304372cb77bc78b0b92c36e0eb50cfe46
andrearomeo04 <pisco004@gmail.com>
2024-10-15 19:15:50 +0200

    [UPD] cicled type select on edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -23,8 +23,9 @@
                         <label for="" class="control-label">Tipologia Progetto</label>
                         <select name="type_id" class="form-select" id="" required>
                             <option value="">seleziona la tipologia</option>
-                            <option value="">front-end</option>
-                            <option value="">back-end</option>
+                            @foreach ($types as $type)
+                            <option value="{{ $type->id }}" @selected($type->id == old('type_id', $project->type ? $project->type->id : ''))>{{ $type->title }}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="col-12">
